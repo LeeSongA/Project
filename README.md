0a38ddb179cff2a774e5c26574231d48ef95a167
LeeSongA <thddk0317@nate.com>
2018-04-24 18:45:02 +0900

    make dialog for login

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 Insert flag into chrome  
 - **--allow-file-access-from-files**  
 - **--disable-web-security --user-data-dir**   
+
 <br>
+
 ## Reference  
+
 - Background Image  
 ```
 https://pixabay.com/ko/나무-공기-배경-마실-것-블로그-블로거-휴식-사업-3157395/
--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
 		location.href = "main.html";
 	});
 	setTimeout(function(){ 
-		$("#div_shortcutList a").attr("href", "https://github.com/LeeSongA/Project");
-		$("#div_shortcutList a").attr("target", "_blank");
+		$("#div_shortcutList a:nth-of-type(1)").attr("href", "javascript:login();");
+		$("#div_shortcutList a:nth-of-type(2)").attr("href", "https://github.com/LeeSongA/Project");
+		$("#div_shortcutList a:nth-of-type(2)").attr("target", "_blank");
 		$("#div_contents").css("height", $(window).height()-$("#div_menu").height()-10);
 		for(var i=1;i<=5;i++)
 			$("#div_menuList a:nth-of-type("+i+")").attr("href", "javascript:selectMenu("+i+");");
@@ -19,4 +20,13 @@ function selectMenu(number){
 		$("#div_contents").load("introduce.html");
 	else if(number == 4)
 		$("#div_contents").load("member.html");
+}
+
+function login(){
+	$('#div_login').css('display','block');
+	$('#div_mask').css('display','block');
+}
+function close(){
+	$('#div_login').css('display','none');
+	$('#div_mask').css('display','none');
 }
